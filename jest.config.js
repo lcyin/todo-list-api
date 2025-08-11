6ad38bdb0a5d113b244a1dcebc3182539f7c6d60
lcyin <laichunyinkevin@gmailcom>
2025-08-11 23:30:56 +0800

    feat: add Jest setup file for database cleanup and test utilities

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/index.ts"],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "html"],
-  setupFilesAfterEnv: ["<rootDir>/src/__tests__/setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
   testTimeout: 10000,
   forceExit: true,
   clearMocks: true,
